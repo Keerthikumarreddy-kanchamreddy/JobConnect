05b1b314186be67eead8ec75df4d507558c6e72c
Keerthikumarreddy-kanchamreddy <122507926+Keerthikumarreddy-kanchamreddy@users.noreply.github.com>
2024-10-29 19:25:19 -0400

    User service class to add a new user to database

--- jobConnect/src/main/java/com/application/jobConnect/controller/UsersController.java
+++ jobConnect/src/main/java/com/application/jobConnect/controller/UsersController.java
@@ -2,11 +2,14 @@ package com.application.jobConnect.controller;
 
 import com.application.jobConnect.entity.Users;
 import com.application.jobConnect.entity.UsersType;
+import com.application.jobConnect.services.UsersService;
 import com.application.jobConnect.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -14,11 +17,14 @@ import java.util.List;
 public class UsersController {
 
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
 
     @Autowired
-    public UsersController(UsersTypeService usersTypeService){
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService){
         this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
     }
+
 
     @GetMapping("/register")
     public String register(Model model){
@@ -26,5 +32,12 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users){
+    //    System.out.println("User:: " + users);
+        usersService.addNew(users);
+        return "dashboard";
     }
 }
