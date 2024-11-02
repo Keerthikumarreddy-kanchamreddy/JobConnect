5390b639b28b86ead0c2fa1cb00291398d39fbca
Keerthikumarreddy-kanchamreddy <122507926+Keerthikumarreddy-kanchamreddy@users.noreply.github.com>
2024-11-02 18:35:15 -0400

    setup custom authentication success handler

--- jobConnect/src/main/java/com/application/jobConnect/config/WebSecurityConfig.java
+++ jobConnect/src/main/java/com/application/jobConnect/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,13 +17,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Autowired
-    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public WebSecurityConfig(CustomUserDetailsService customUserDetailsService, CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
         this.customUserDetailsService = customUserDetailsService;
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
     }
-
-
 
     private final String[] publicUrl={"/",
             "/global-search/**",
@@ -49,6 +50,10 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests(auth -> {auth.requestMatchers(publicUrl).permitAll();
             auth.anyRequest().authenticated();
         });
+
+        http.formLogin(form -> form.loginPage("/login").permitAll().successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> {logout.logoutUrl("/logout");
+                logout.logoutSuccessUrl("/");}).cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable());
         return http.build();
     }
 
