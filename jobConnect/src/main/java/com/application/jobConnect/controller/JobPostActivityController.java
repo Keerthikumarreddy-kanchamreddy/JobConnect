a045158b73d92c412c5bb3c878838ac2c58a6421
Keerthikumarreddy-kanchamreddy <122507926+Keerthikumarreddy-kanchamreddy@users.noreply.github.com>
2024-11-06 23:57:52 -0500

    Display and retrieve jobs

--- jobConnect/src/main/java/com/application/jobConnect/controller/JobPostActivityController.java
+++ jobConnect/src/main/java/com/application/jobConnect/controller/JobPostActivityController.java
@@ -1,12 +1,15 @@
 package com.application.jobConnect.controller;
 
 import com.application.jobConnect.entity.JobPostActivity;
+import com.application.jobConnect.entity.RecruiterJobsDto;
+import com.application.jobConnect.entity.RecruiterProfile;
 import com.application.jobConnect.entity.Users;
 import com.application.jobConnect.services.JobPostActivityService;
 import com.application.jobConnect.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -34,6 +38,11 @@ public class JobPostActivityController {
         if(!(authentication instanceof AnonymousAuthenticationToken)){
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile)currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
