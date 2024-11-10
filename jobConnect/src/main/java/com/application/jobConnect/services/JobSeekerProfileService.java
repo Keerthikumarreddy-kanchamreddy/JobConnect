81324fe37d6bafff0c9427816051c69342195c58
Keerthikumarreddy-kanchamreddy <122507926+Keerthikumarreddy-kanchamreddy@users.noreply.github.com>
2024-11-10 12:48:24 -0500

    Add support for file upload - job seeker profile image and resume

--- jobConnect/src/main/java/com/application/jobConnect/services/JobSeekerProfileService.java
+++ jobConnect/src/main/java/com/application/jobConnect/services/JobSeekerProfileService.java
@@ -2,6 +2,7 @@ package com.application.jobConnect.services;
 
 import com.application.jobConnect.entity.JobSeekerProfile;
 import com.application.jobConnect.repository.JobSeekerProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,11 +12,16 @@ public class JobSeekerProfileService {
 
     private final JobSeekerProfileRepository jobSeekerProfileRepository;
 
+    @Autowired
     public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
         this.jobSeekerProfileRepository = jobSeekerProfileRepository;
     }
 
     public Optional<JobSeekerProfile> getOne(Integer id){
         return jobSeekerProfileRepository.findById(id);
+    }
+
+    public JobSeekerProfile addNew(JobSeekerProfile jobSeekerProfile) {
+        return jobSeekerProfileRepository.save(jobSeekerProfile);
     }
 }
